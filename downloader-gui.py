7250a474d779696a61cf72ec4786fdf5446e6cde
Jing Jie <evelyst21@gmail.com>
2018-06-06 17:03:25 +0800

    Add files via upload

---
+++ downloader-gui.py
@@ -0,0 +1,101 @@
+from tkinter import *
+from tkinter import filedialog
+import downloader, webbrowser, _thread
+
+# Declare a GLOBAL Font
+FONT = ('Arial', 14, 'bold')
+
+class GUIDownloader(object):
+    def __init__(self):
+
+        # Let's make some fun callbacks :D
+        def urlCallback(url):
+            webbrowser.open_new(r"https://github.com/evelystria")
+
+        self.username = ''
+        self.password = ''
+        self.module = ''
+        self.folder = ''
+
+        root = Tk()
+        root.withdraw()
+        root.update_idletasks()
+
+        # Get screen size
+        x = (root.winfo_screenwidth() - root.winfo_reqwidth()) / 2
+        y = (root.winfo_screenheight() - root.winfo_reqheight()) / 2
+        root.geometry("%dx%d" % (x, y))
+        root.deiconify()
+
+        root.title('RP Exam Downloader')
+        root.resizable(0, 0)
+
+        self.top = Frame(root)
+        self.top.grid(row=0, column=0, padx=220, pady=50)
+        self.top.columnconfigure(0, weight=1)
+        self.top.rowconfigure(0, weight=1)
+
+        Label_Title = Label(self.top, text='RP Exam Downloader', font=('Arial', 28, 'bold'))
+        Label_Title.grid(row=1, column=0, columnspan=3, padx=20, pady=20)
+
+        Label_Login = Label(self.top, text='Student ID:', font=FONT)
+        Label_Login.grid(row=2, column=0)
+        self.LoginField = Entry(self.top, bd=2, textvariable=self.username)
+        self.LoginField.grid(row=2, column=1)
+
+        Label_Password = Label(self.top, text='Password:', font=FONT)
+        Label_Password.grid(row=3, column=0)
+        self.PasswordField = Entry(self.top, bd=2, show='*', textvariable=self.password)
+        self.PasswordField.grid(row=3, column=1)
+
+        Label_Module = Label(self.top, text='Module:', font=FONT)
+        Label_Module.grid(row=4, column=0)
+        self.ModuleField = Entry(self.top, bd=2, textvariable=self.module)
+        self.ModuleField.grid(row=4, column=1)
+
+        Label_Folder = Label(self.top, text='Save To Location:', font=FONT)
+        Label_Folder.grid(row=5, column=0)
+        self.FolderField = Entry(self.top, bd=2, textvariable=self.folder, state='disabled')
+        self.FolderField.grid(row=5, column=1)
+        Button_Folder = Button(self.top, text='Browse', command=self.locateFolder)
+        Button_Folder.grid(row=5, column=2)
+
+        self.StatusField = Label(self.top, text='- Developed by Jing Jie, SOI-DIDM')
+        self.StatusField.grid(row=6, columnspan=3, pady=(20,0))
+        linkLabel = Label(self.top, text='https://github.com/evelystria', fg="blue", cursor="hand2")
+        linkLabel.grid(row=7, columnspan=3, padx=20)
+        linkLabel.bind("<Button-1>", urlCallback)
+
+        startButton = Button(self.top, text='Get Papers!', command=self.beginDownload)
+        startButton.grid(row=8, columnspan=3, padx=20, pady=20)
+
+        # Run the app
+        root.mainloop()
+
+    def locateFolder(self):
+        self.folder = filedialog.askdirectory(mustexist=False, parent=self.top, title='Choose a folder')
+        self.FolderField.configure(state='normal')
+        self.FolderField.delete(0, END)
+        self.FolderField.insert(0, self.folder)
+        self.FolderField.configure(state='disabled')
+
+    def beginDownload(self):
+        username = self.LoginField.get()
+        password = self.PasswordField.get()
+        module = self.ModuleField.get()
+        folder = self.FolderField.get()
+        DLR = downloader.downloader('GUI')
+
+        _thread.start_new_thread(DLR.beginDLR, (username, password, module, folder, self.updateStatus))
+
+    def updateStatus(self, dispatch, type='normal'):
+        self.StatusField['text'] = dispatch
+        if type == 'success':
+            self.StatusField['fg'] = 'green'
+        elif type == 'error':
+            self.StatusField['fg'] = 'red'
+        else:
+            self.StatusField['fg'] = 'blue'
+
+if __name__ == '__main__':
+    GUIDownloader()
---
+++ downloader.py
@@ -0,0 +1,104 @@
+import urllib.parse, urllib.request, os, sys, requests, re
+from requests_ntlm import HttpNtlmAuth
+from bs4 import BeautifulSoup as bs
+
+class downloader(object):
+
+    def __init__(self, mode):
+        self.mode = mode
+
+    def beginDLR(self, username, password, module, folder, updateStatus):
+
+        updateStatus('Connecting to RP Library . . .')
+
+        # params
+        site = 'intranet.rp.edu.sg'
+        page = '/lib/LibraryServices/_layouts/15/start.aspx'+'#'+'/eRepository/Forms/AllItems.aspx'
+        url = 'http://intranet.rp.edu.sg/lib/LibraryServices/eRepository/Examination%20Paper/C105%20Introduction%20to%20Programming'
+
+        session = requests.Session()
+        session.auth = HttpNtlmAuth(username, password)
+
+        headers = {
+            'accept' : 'application/json;odata=verbose',
+            'content-type' : 'application/json;odata=verbose',
+            'odata' : 'verbose',
+            'X-RequestForceAuthentication' : 'true'
+        }
+
+        try:
+
+            # Make a session request to the server
+            # First, to authenticate the user's credentials if he/she is connected to RP's VPN
+            r = session.get(url, headers=headers)
+
+            # If status_code == ok or 200 means user have successfully connected to RP's VPN
+            if (r.status_code == 200):
+
+                updateStatus("Successfully connected to online library...", 'success')
+
+                #-- Search for module
+                mod_url = 'https://libopac.rp.edu.sg/client/en_GB/home/search/results?qu=' + module
+
+                mod_r = requests.get(mod_url)
+                mod_c = mod_r.content
+
+                mod_soup = bs(mod_c, "lxml")
+
+                count = 0
+
+                #-- We exclude everything except "intranet" as it is where the resources are located
+                for mod_tag in mod_soup.findAll('a', href=re.compile("intranet")):
+                    mod_temp_url = mod_tag['href']
+                    count = count + 1
+
+                if count > 0:
+                    mod_new_url = mod_temp_url.replace(" ", "%20")
+                    updateStatus(("Module %s found, begin download query..." % module), 'success')
+
+                    #-- Get folder destination
+                    path = folder
+
+                    #-- Read the request(url) in bytes form, used to write files later
+                    # Make a session request to the server
+                    r = session.get(mod_new_url, headers=headers)
+                    c = r.content
+                    soup = bs(c, "lxml")
+
+                    #-- Downloader
+                    i = 0
+
+                    for tag in soup.findAll('a', href=True):
+                        tag['href'] = urllib.parse.urljoin(url, tag['href'])
+
+                        if os.path.splitext(os.path.basename(tag['href']))[1] == '.pdf':
+                            current = session.get((tag['href'])).content
+                            updateStatus("Downloading: %s" %(os.path.basename(tag['href'])), 'success')
+
+                            try:
+
+                                f = open(path + "\\" + os.path.basename(tag['href']), "wb")
+                                f.write(current)
+                                f.close()
+                                i+=1
+
+                            except Exception as e:
+
+                                updateStatus("Unable to use destination!", 'error')
+
+                    updateStatus("Downloaded %d files" %(i), 'success')
+
+                else:
+                    updateStatus("Sorry, the module is not found in our database.", 'error')
+
+            # If status_code returns 401 -> user's credentials failed therefore not authorized
+            else:
+                updateStatus("Authorization failed. Please ensure you have the right credentials.", 'error')
+
+            return
+
+        # If user is not connected to RP's VPN, this handler will be raised
+        except requests.exceptions.ConnectionError:
+
+            updateStatus("Unable to connect to destination. Ensure that you're connected to RP's VPN...", 'error')
+            return
